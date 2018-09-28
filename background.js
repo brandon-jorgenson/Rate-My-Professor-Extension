b8e86c3d838e4c3511778416c84387a28965048e
Brandon Jorgenson <brandonjorg123@gmail.com>
2018-09-27 21:02:14 -0600

    "push"

--- background.js
+++ background.js
@@ -1,1 +1,5 @@
-"hello world"
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.storage.sync.set({color: '#3aa757'}, function() {
+      console.log("The color is green.");
+    });
+  });
