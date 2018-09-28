7d2bde023e604518f30bcef2b11df48975680df3
Brandon Jorgenson <brandonjorg123@gmail.com>
2018-09-28 07:33:38 -0600

    "connects with api"

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,7 @@
 
  var sectionsRow = $(".sectionsRow");
  for(var i=0; i < sectionsRow.length; i++) {
-     var myurl = "http://search.mtvnservices.com/typeahead/suggest/?solrformat=true&rows=20&callback=noCB&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=20&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s&fq=&q=";
+     var myurl = "https://search.mtvnservices.com/typeahead/suggest/?solrformat=true&rows=20&callback=noCB&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=20&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s&fq=&q=";
      var element = sectionsRow.eq(i);
      element.find("td").eq(17).css({"color": "red", "border": "2px solid red"});
      var fullName = element.find("td").eq(17).text()
@@ -11,7 +11,17 @@
      lastName = lastName.toLowerCase();
      lastName = lastName.trim();
      firstName = firstName.toLowerCase();
-     myurl += firstName + "+" + lastName + "+AND+schoolid_s%3A135";
+     myurl1 = myurl + firstName + "+" + lastName + "+AND+schoolid_s%3A135";
+     var xhr = new XMLHttpRequest();
+     xhr.open("GET", myurl1, true);
+     xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+        // JSON.parse does not evaluate the attacker's scripts.
+       // var resp = JSON.parse(xhr.responseText);
+        console.log("success");
+  }
+}
+xhr.send();
      console.log(fullName);
      console.log(firstName);
      console.log(lastName);
