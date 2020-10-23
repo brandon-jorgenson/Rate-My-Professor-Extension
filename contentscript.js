638d3c27b91a300b13f183d3a941916281592514
Calvin Wu <calvinwu4@gmail.com>
2020-10-23 00:41:29 -0400

    Get ratings from all pages

--- contentscript.js
+++ contentscript.js
@@ -40,7 +40,7 @@ function GetProfessorRating(myurl1, element, lastName, firstName, middleName, ru
             element.setAttribute('target', '_blank');
 
             // if (profRating != undefined) {
-                const allprofRatingsURL = "https://www.ratemyprofessors.com/paginate/professors/ratings?tid=" + profID + "&page=0&max=20";
+                let allprofRatingsURL = "https://www.ratemyprofessors.com/paginate/professors/ratings?tid=" + profID + "&page=0&max=20";
                 AddTooltip(element, allprofRatingsURL, realFirstName, realLastName, profRating, numRatings, easyRating, dept);
             // }
         } else {
@@ -68,89 +68,100 @@ function GetProfessorRating(myurl1, element, lastName, firstName, middleName, ru
 }
 
 function AddTooltip(element, allprofRatingsURL, realFirstName, realLastName, profRating, numRatings, easyRating, dept) {
-    chrome.runtime.sendMessage({ url: allprofRatingsURL, type: "tooltip" }, function (response) {
-        const resp = response.JSONresponse;
-        //Build content for professor tooltip
-        let wouldTakeAgain = 0;
-        let wouldTakeAgainNACount = 0;
-        let mostHelpfulReview;
-        let helpCount;
-        let notHelpCount;
-        let wouldTakeAgainText;
-        let easyRatingText;
+    let ratings = [];
+    function getRatings(url){
+        chrome.runtime.sendMessage({ url: url, type: "tooltip" }, function (response) { 
+            ratings = ratings.concat(response.JSONresponse.ratings);
+            var remaining = response.JSONresponse.remaining;
+            let pageNum = parseInt(new URLSearchParams(url).get('page'));
+            if(remaining !== 0) { 
+                // Get all ratings by going through all the pages
+                getRatings(url.replace(`page=${pageNum}`, `page=${pageNum + 1}`));
+            }
+            else{
+                // Build content for professor tooltip
+                let wouldTakeAgain = 0;
+                let wouldTakeAgainNACount = 0;
+                let mostHelpfulReview;
+                let helpCount;
+                let notHelpCount;
+                let wouldTakeAgainText;
+                let easyRatingText;
 
-        const div = document.createElement("div");
-        const title = document.createElement("h3");
-        title.textContent = "Rate My Professor Details";
-        const professorText = document.createElement("p");
-        const avgRatingText = document.createElement("p");
-        avgRatingText.textContent = `Overall rating: ${profRating ? profRating : 'N/A'}/5`
-        const numRatingsText = document.createElement("p");
-        numRatingsText.textContent = `Number of Ratings: ${numRatings}`
-        professorText.textContent = `${realFirstName} ${realLastName}, Professor in the ${dept} department`;
-        div.appendChild(title);
-        div.appendChild(professorText);
-        div.appendChild(avgRatingText);
-        div.appendChild(numRatingsText);
+                const div = document.createElement("div");
+                const title = document.createElement("h3");
+                title.textContent = "Rate My Professor Details";
+                const professorText = document.createElement("p");
+                const avgRatingText = document.createElement("p");
+                avgRatingText.textContent = `Overall rating: ${profRating ? profRating : 'N/A'}/5`
+                const numRatingsText = document.createElement("p");
+                numRatingsText.textContent = `Number of Ratings: ${numRatings}`
+                professorText.textContent = `${realFirstName} ${realLastName}, Professor in the ${dept} department`;
+                div.appendChild(title);
+                div.appendChild(professorText);
+                div.appendChild(avgRatingText);
+                div.appendChild(numRatingsText);
 
-        if (resp.ratings.length > 0) {
-            
-            for (let i = 0; i < resp.ratings.length; i++) {
-                if (resp.ratings[i].rWouldTakeAgain === "Yes") {
-                    wouldTakeAgain++;
-                } else if (resp.ratings[i].rWouldTakeAgain === "N/A") {
-                    wouldTakeAgainNACount++;
+                if (ratings.length > 0) {
+                    for (let i = 0; i < ratings.length; i++) {
+                        if (ratings[i].rWouldTakeAgain === "Yes") {
+                            wouldTakeAgain++;
+                        } else if (ratings[i].rWouldTakeAgain === "N/A") {
+                            wouldTakeAgainNACount++;
+                        }
+                    }
+                    if(ratings.length > 1) {
+                        ratings.sort(function(a,b) { return new Date(b.rDate) - new Date(a.rDate) });
+                        ratings.sort(function(a,b) { return (b.helpCount - b.notHelpCount) - (a.helpCount - a.notHelpCount) });
+                        mostHelpfulReview = ratings[0];
+                        helpCount = mostHelpfulReview.helpCount;
+                        notHelpCount = mostHelpfulReview.notHelpCount;
+                    }
+                    if (ratings.length >= 8 && wouldTakeAgainNACount < (ratings.length / 2)) {
+                        wouldTakeAgain = ((wouldTakeAgain / (ratings.length - wouldTakeAgainNACount)) * 100).toFixed(0).toString() + "%";
+                    } else {
+                        wouldTakeAgain = "N/A";
+                    }
+                    wouldTakeAgainText = document.createElement("p");
+                    wouldTakeAgainText.textContent = "Would take again: " + wouldTakeAgain;
+                    easyRatingText = document.createElement("p");
+                    easyRatingText.textContent = `Level of Difficulty: ${easyRating}`;
+                    div.appendChild(easyRatingText);
+                    div.appendChild(wouldTakeAgainText);
                 }
-            }
-            if(resp.ratings.length > 1) {
-                resp.ratings.sort(function(a,b) { return new Date(b.rDate) - new Date(a.rDate) });
-                resp.ratings.sort(function(a,b) { return (b.helpCount - b.notHelpCount) - (a.helpCount - a.notHelpCount) });
-                mostHelpfulReview = resp.ratings[0];
-                helpCount = mostHelpfulReview.helpCount;
-                notHelpCount = mostHelpfulReview.notHelpCount;
-            }
-            if (resp.ratings.length >= 8 && wouldTakeAgainNACount < (resp.ratings.length / 2)) {
-                wouldTakeAgain = ((wouldTakeAgain / (resp.ratings.length - wouldTakeAgainNACount)) * 100).toFixed(0).toString() + "%";
-            } else {
-                wouldTakeAgain = "N/A";
-            }
-            wouldTakeAgainText = document.createElement("p");
-            wouldTakeAgainText.textContent = "Would take again: " + wouldTakeAgain;
-            easyRatingText = document.createElement("p");
-            easyRatingText.textContent = `Level of Difficulty: ${easyRating}`;
-            div.appendChild(easyRatingText);
-            div.appendChild(wouldTakeAgainText);
-        }
-        if (mostHelpfulReview) {
-            const classText = document.createElement("p");
-            classText.textContent = "Most Helpful Rating: " + mostHelpfulReview.rClass;
-            const dateText = document.createElement("p");
-            dateText.textContent = mostHelpfulReview.rDate;
-            const commentText = document.createElement("p");
-            commentText.textContent = mostHelpfulReview.rComments;
-            commentText.classList.add('paragraph');
-            const upvotesText = document.createElement("p");
-            upvotesText.textContent = `👍${helpCount} 👎${notHelpCount}`;
-            div.appendChild(classText);
-            div.appendChild(dateText);
-            div.appendChild(commentText);
-            div.appendChild(upvotesText);
-        }
-        element.class = "tooltip";
-        element.addEventListener("mouseenter", function () {
-            //Only create tooltip once
-            if (!$(element).hasClass('tooltipstered')) {
-                $(this)
-                    .tooltipster({
-                        animation: 'grow',
-                        theme: 'tooltipster-default',
-                        side: 'left',
-                        content: div,
-                        contentAsHTML: true,
-                        delay: 100
-                    })
-                    .tooltipster('show');
+                if (mostHelpfulReview) {
+                    const classText = document.createElement("p");
+                    classText.textContent = "Most Helpful Rating: " + mostHelpfulReview.rClass;
+                    const dateText = document.createElement("p");
+                    dateText.textContent = mostHelpfulReview.rDate;
+                    const commentText = document.createElement("p");
+                    commentText.textContent = mostHelpfulReview.rComments;
+                    commentText.classList.add('paragraph');
+                    const upvotesText = document.createElement("p");
+                    upvotesText.textContent = `👍${helpCount} 👎${notHelpCount}`;
+                    div.appendChild(classText);
+                    div.appendChild(dateText);
+                    div.appendChild(commentText);
+                    div.appendChild(upvotesText);
+                }
+                element.class = "tooltip";
+                element.addEventListener("mouseenter", function () {
+                    //Only create tooltip once
+                    if (!$(element).hasClass('tooltipstered')) {
+                        $(this)
+                            .tooltipster({
+                                animation: 'grow',
+                                theme: 'tooltipster-default',
+                                side: 'left',
+                                content: div,
+                                contentAsHTML: true,
+                                delay: 100
+                            })
+                            .tooltipster('show');
+                    }
+                });
             }
         });
-    });
+    }
+    getRatings(allprofRatingsURL)
 }
